150ef2122da57dfae12168f3a0c349bab80197a8
houdevile <32546134+eymardhouds@users.noreply.github.com>
2018-04-15 18:33:38 +0200

    changes

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ from lecture_chiffres.read_bibs import read_with_crnn
 
 # Read a bib number
 print("Using CRNN module")
-path = 'samples/bib/bib1.png'
+path = 'samples/bib/69.png'
 C = read_with_crnn(path)
 c = C.predict()
 print(c)

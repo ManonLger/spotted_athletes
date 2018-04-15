fd3d8ec17ea9beb94fff356610575c5ee1d9d3b8
De Revel Emmanuel <emmanuel.de-revel@student.ecp.fr>
2018-04-15 20:19:48 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,15 +1,14 @@
 from lecture_chiffres.read_bibs import read_with_crnn
 from detection_chiffres.detection import Dossard
-from matplotlib import pyplot as plt
 
 
 #Detect number on bib
 print("Using Number detection module")
-dossard=Dossard("dossard_raid_2.jpg")
-number=dossard.numberDetection()
-plt.imshow(number)
-plt.show()
-print("Number detected")
+for bib_name in os.listdir("detection_chiffres/detection_inputs"):
+    dossard=Dossard(os.path.join("detection_chiffres/detection_inputs", bib_name))
+    number=dossard.numberDetection()
+    cv2.imwrite("detection_chiffres/detection_outputs/"+ bib_name+ "_number_detected.jpg", number)
+    print("Number detected")
 
 
 # Read a bib number
